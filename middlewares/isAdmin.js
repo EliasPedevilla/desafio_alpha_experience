fdc773dce89481a7ea09d0df99c4693dea4edb26
Elias Pedevilla <pedevillae@gmail.com>
2023-08-03 18:12:05 -0300

    fix isAdmin validation

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -3,7 +3,7 @@ const { error } = require("../helpers")
 module.exports = (req, res, next) => {
   const { role } = req.userData
 
-  return (role === "admin")
+  return (!role === "admin")
     ? error({ res, message: 'request unauthorized, you need to be admin' })
     : next()
 }
